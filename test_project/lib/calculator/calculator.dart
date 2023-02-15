9ada1db4bb2d5dea2bff5560a0e2964c0a1b9bb6
mjdarvishi <mjdarivshi1374@gamil.com>
2023-02-15 19:22:37 +0330

    statefull awidget

--- test_project/lib/calculator/calculator.dart
+++ test_project/lib/calculator/calculator.dart
@@ -10,87 +10,98 @@ class CalculatorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        leading: IconButton(
+          onPressed: () {
+              Navigator.pop(context);
+          },
+          icon: Icon(Icons.keyboard_backspace,color: Colors.white,),
+        ),
+        title: Text('Calculator'),
+      ),
         body: SafeArea(
-            child: Column(
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: 200,
-          margin: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-              color: Colors.yellow[500],
-              borderRadius: BorderRadius.circular(10)),
-        ),
-        const Spacer(),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-          KeyWidget('+', () {
-            operator = '+';
-          }),
-          KeyWidget('*', () {
-            operator = '*';
-          }),
-          KeyWidget('-', () {
-            operator = '-';
-          }),
-          KeyWidget('=', () {
-            switch (operator) {
-              case '+':
-                print(int.parse(fitstNumber) + int.parse(sNumber));
-                break;
-              case '-':
-                print(int.parse(fitstNumber) - int.parse(sNumber));
-                break;
-              case '*':
-                print(int.parse(fitstNumber) * int.parse(sNumber));
-                break;
-            }
-          })
-        ]),
-        const SizedBox(
-          height: 20,
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-          KeyWidget('1', () {
-            handler('1');
-          }),
-          KeyWidget('2', () {
-            handler('2');
-          }),
-          KeyWidget('3', () {
-            handler('3');
-          })
-        ]),
-        const SizedBox(
-          height: 10,
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-          KeyWidget('4', () {
-            handler('4');
-          }),
-          KeyWidget('5', () {
-            handler('5');
-          }),
-          KeyWidget('6', () {
-            handler('6');
-          })
-        ]),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            KeyWidget('7', () {
-              handler('7');
-            }),
-            KeyWidget('8', () {
-              handler('8');
-            }),
-            KeyWidget('9', () {
-              handler('9');
-            })
-          ]),
-        ),
-      ],
-    )));
+            child:Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.yellow[500],
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            const Spacer(),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              KeyWidget('+', () {
+                operator = '+';
+              }),
+              KeyWidget('*', () {
+                operator = '*';
+              }),
+              KeyWidget('-', () {
+                operator = '-';
+              }),
+              KeyWidget('=', () {
+                switch (operator) {
+                  case '+':
+                    print(int.parse(fitstNumber) + int.parse(sNumber));
+                    break;
+                  case '-':
+                    print(int.parse(fitstNumber) - int.parse(sNumber));
+                    break;
+                  case '*':
+                    print(int.parse(fitstNumber) * int.parse(sNumber));
+                    break;
+                }
+              })
+            ]),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              KeyWidget('1', () {
+                handler('1');
+              }),
+              KeyWidget('2', () {
+                handler('2');
+              }),
+              KeyWidget('3', () {
+                handler('3');
+              })
+            ]),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              KeyWidget('4', () {
+                handler('4');
+              }),
+              KeyWidget('5', () {
+                handler('5');
+              }),
+              KeyWidget('6', () {
+                handler('6');
+              })
+            ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    KeyWidget('7', () {
+                      handler('7');
+                    }),
+                    KeyWidget('8', () {
+                      handler('8');
+                    }),
+                    KeyWidget('9', () {
+                      handler('9');
+                    })
+                  ]),
+            ),
+          ],
+        )));
   }
 
   void handler(String value) {
@@ -99,8 +110,8 @@ class CalculatorWidget extends StatelessWidget {
     } else {
       sNumber += value;
     }
-   // print('first number is $fitstNumber');
-   // print('second number is $sNumber');
-   // print('operator number is $operator');
+    // print('first number is $fitstNumber');
+    // print('second number is $sNumber');
+    // print('operator number is $operator');
   }
 }
---
+++ test_project/lib/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/calculator/calculator.dart';
+import 'package:test_project/statefull/test.dart';
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Barbod Application',style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.grey,
+       // leading: Icon(Icons.arrow_back_ios_new_outlined),
+        actions: [
+       //   IconButton(onPressed: () {}, icon: Icon(Icons.ac_unit)),
+       //   Icon(Icons.home, color: Colors.green, size: 25),
+       //   Icon(Icons.notifications)
+        ],
+      ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder:(context) => CalculatorWidget(),));
+              },
+              child: Icon(Icons.calculate,size: 70,color: Colors.blueGrey,)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder:(context) => TestWidget(),));
+              },
+              child: Icon(Icons.transfer_within_a_station,size: 70,color: Colors.blueGrey,)),
+        )
+      ],),
+    );
+  }
+}
